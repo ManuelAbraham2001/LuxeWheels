1b1d267a11bb893acb7666f1464564f5a9a5c8e9
manu <manu.20012009@gmail.com>
2023-10-13 20:45:33 -0300

    initial template

---
+++ Backend/src/main/java/com/LuxeWheels/LuxeWheelsApplication.java
@@ -0,0 +1,13 @@
+package com.LuxeWheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxeWheelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxeWheelsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/LuxeWheels/LuxeWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LuxeWheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxeWheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
