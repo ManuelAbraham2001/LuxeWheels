a2e1602c4ed82dc6760a4e49666006b1eda47edd
GisselFerrin <asolysombra20111@gmail.com>
2023-10-23 19:24:14 -0300

    Background-Body/Footer

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -7,12 +7,14 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer'>
-        <p>Copyright</p>
-        
+        <p>Copyright 2023</p>
+        <div className='iconos'>
         <img className='icon' src="/images/ico-facebook.png" alt='facebook-logo' />
         <img  className='icon'src="/images/ico-instagram.png" alt='instagram-logo' />
         <img  className='icon'src="/images/ico-tiktok.png" alt='tiktok-logo' />
         <img  className='icon'src="/images/ico-whatsapp.png" alt='whatsapp-logo' />
+    </div>
+    
     </footer>
   )
 }
