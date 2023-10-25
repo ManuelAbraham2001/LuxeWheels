f85765a88439a8f9ebb25e3087d3ac60200342f3
GisselFerrin <asolysombra20111@gmail.com>
2023-10-24 21:23:09 -0300

    logo-footer

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -7,8 +7,12 @@ import "./styles/Footer.css"
 const Footer = () => {
   return (
     <footer className='footer'>
+        
+        <div className="logo-container">
+        <img className="logo"src="/images/Logo_dark1.png" alt="Autos de alquiler" />
+        </div>
         <p>Copyright 2023</p>
-        <div className='iconos'>
+      <div className='iconos'>
         <img className='icon' src="/images/ico-facebook.png" alt='facebook-logo' />
         <img  className='icon'src="/images/ico-instagram.png" alt='instagram-logo' />
         <img  className='icon'src="/images/ico-tiktok.png" alt='tiktok-logo' />
