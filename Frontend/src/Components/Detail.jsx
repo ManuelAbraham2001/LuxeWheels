c3808dbe067e1ea0319b4fbf06689e06e4dc7ad0
Gonzalo Mejibar <gonzalomejibar@gmail.com>
2023-10-26 19:24:40 -0300

    mensaje de descripcion en details

--- Frontend/src/Components/Detail.jsx
+++ Frontend/src/Components/Detail.jsx
@@ -9,7 +9,8 @@ const Detail = () => {
     <div className="main-container">
       <h2>modelo</h2>
       <Gallery/>
-      <p>descripcion</p>
+      <p>descripcion:</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa odio exercitationem? Possimus cupiditate, quae perspiciatis sapiente est deserunt nam quasi incidunt animi explicabo reiciendis tenetur ab, similique et eius. Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet doloremque sit natus. Fugiat inventore dicta cum mollitia possimus ipsam non delectus qui est, officiis minus ducimus, totam laudantium? In, quas! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed dolore cum molestiae accusamus non unde, architecto, esse vero labore recusandae voluptates ad quam maiores aliquam tempore necessitatibus perspiciatis nesciunt assumenda? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis aliquid odit optio deserunt! Veniam molestias recusandae quis minima, optio voluptatem eveniet mollitia minus quod, fugit, officia natus adipisci! Exercitationem?</p>
     </div>
   );
 };
