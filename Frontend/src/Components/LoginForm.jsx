fdc6b13bdea349d2987ed522f480c29ad8680d88
Gonzalo Mejibar <gonzalomejibar@gmail.com>
2023-11-21 14:22:35 -0300

    mensaje de error en validaciones

--- Frontend/src/Components/LoginForm.jsx
+++ Frontend/src/Components/LoginForm.jsx
@@ -13,9 +13,40 @@ const LoginForm = () => {
   const navigate = useNavigate();
 
 
+  const [enviado, setEnviado] = useState(false);
+  const [errorEmail, setErrorEmail] =useState(false);
+  const [errorPassword, setErrorPassword] = useState(false)
+
+  const validarEmail = (email) => {
+
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+      return regexEmail.test(email);
+  }
+  
   const handleLogin = () => {
     // const isAuthenticationSuccessful = email === 'usuario@example.com' && password === '1234';
       // Cambia el estado de la sesión
+    setEnviado(false)
+    setErrorEmail(false)
+    setErrorPassword(false)
+
+    if(validarEmail(email) == true && password.length >= 5) {
+      setEnviado(true)
+      setErrorEmail(false)
+      setErrorPassword(false)
+
+    } else if (validarEmail(email) == false && password.length >=5) {
+      setErrorEmail(true)
+      setErrorPassword(false)
+    }else if(validarEmail(email) == true && password.length < 5){
+      setErrorEmail(false)
+      setErrorPassword(true)
+    }else if(validarEmail(email) == false && password.length < 5){
+      setErrorEmail(true)
+      setErrorPassword(true)
+    }
+
       dispatch({ type: 'LOGIN',payload: {
         user: {
           email: email,
@@ -25,6 +56,10 @@ const LoginForm = () => {
       console.log(`Iniciar sesión con ${email} y ${password}`);
 
   };
+
+  const errorEmailMessage =  <h3 style={{color: 'red'}}>Coloque un email valido(Ej:ejemplo@email.com)</h3>;
+  const errorPasswordMessage =  <h3 style={{color: 'red'}}>La contraseña debe contener 5 digitos o más</h3>;
+
 
   return (
     <div className="background-image">
@@ -38,6 +73,8 @@ const LoginForm = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {errorEmail && <p className="error-message">{errorEmailMessage}</p>}
+
           <label className="label-field">Contraseña:</label>
           <input
             className="input-field"
@@ -45,6 +82,8 @@ const LoginForm = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {errorPassword && <p className="error-message">{errorPasswordMessage}</p>}
+
           <button className="submit-button" onClick={handleLogin}>
             Iniciar Sesión
           </button>
--- Frontend/src/Components/RegisterForm.jsx
+++ Frontend/src/Components/RegisterForm.jsx
@@ -30,7 +30,22 @@ const RegisterForm = () => {
   }
 
   const [enviado, setEnviado] = useState(false);
-  const [errors, setError] =useState(false);
+  const [errorNombre, setErrorNombre] =useState(false);
+  const [errorApellido, setErrorApellido] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+  const [errorFechaNacimiento, setErrorFechaNacimiento] = useState(false);
+  const [errorTelefono, setErrorTelefono] = useState(false);
+  const [errorDocumento, setErrorDocumento] = useState(false);
+
+  const errorNombreMessage = <h3 style={{color: 'red'}}> Debe contener al menos dos letras </h3>;
+  const errorApellidoMessage = <h3 style={{color: 'red'}}>Debe contener al menos dos letras y no se permiten caracteres especiales ni números.</h3>;
+  const errorEmailMessage = <h3 style={{color: 'red'}}>Coloque un email valido(Ej:ejemplo@email.com)</h3>;
+  const errorPasswordMessage = <h3 style={{color: 'red'}}>La contraseña debe contener 5 digitos o más</h3>;
+  const errorFechaNacimientoMessage = <h3 style={{color: 'red'}}>Por favor coloque su fecha de nacimiento</h3>;
+  const errorTelefonoMessage = <h3 style={{color: 'red'}}>Por favor verifique los datos nuevamente. Debe contener mas de 5 digitos</h3>;
+  const errorDocumentoMessage = <h3 style={{color: 'red'}}>Debe contener mas de 6 digitos</h3>;
+
 
   const validarEmail = (email) => {
 
@@ -43,13 +58,47 @@ const RegisterForm = () => {
     // Lógica de registro aquí
     e.preventDefault()
     setEnviado(false)
-    setError(false)
+    // setError(false)
+    setErrorNombre(false)
+    setErrorApellido(false)
+    setErrorEmail(false)
+    setErrorPassword(false)
+    setErrorFechaNacimiento(false)
+    setErrorTelefono(false)
+    setErrorDocumento(false)
 
-    if(form.nombre.length > 4 && form.apellido.length > 4 && validarEmail(form.email) == true && form.password.length >= 5) {
+    if(form.nombre.length > 2 && form.apellido.length > 2 && validarEmail(form.email) == true && form.password.length >= 5 && form.fechaNacimiento.length > 0 && form.telefono.length > 5 && form.documento.length > 6) {
       setEnviado(true)
-      setError(false)
-  } else {
-      setError(true)
+      setErrorNombre(false)
+      setErrorApellido(false)
+      setErrorEmail(false)
+      setErrorPassword(false)
+      setErrorFechaNacimiento(false)
+      setErrorTelefono(false)
+      setErrorDocumento(false)
+
+
+  } 
+  if(form.nombre.length < 2){
+    setErrorNombre(true)
+  }
+  if (form.apellido.length < 2){
+    setErrorApellido(true)
+  }
+  if (validarEmail(form.email) == false){
+    setErrorEmail(true)
+  }
+  if(form.password.length < 5){
+    setErrorPassword(true)
+  }
+  if(form.telefono < 6){
+    setErrorTelefono(true)
+  }
+  if(form.fechaNacimiento.length == 0){
+    setErrorFechaNacimiento(true)
+  }
+  if (form.documento.length < 7){
+    setErrorDocumento(true)
   }
 
   setForm({
@@ -64,8 +113,17 @@ const RegisterForm = () => {
       "content-type": "application/json"
     }
   })
+  const errorNombreMessage = <h3 style={{color: 'red'}}> Debe contener al menos dos letras </h3>;
+  const errorApellidoMessage = <h3 style={{color: 'red'}}>Debe contener al menos dos letras y no se permiten caracteres especiales ni números.</h3>;
+  const errorEmailMessage = <h3 style={{color: 'red'}}>Coloque un email valido(Ej:ejemplo@email.com)</h3>;
+  const errorPasswordMessage = <h3 style={{color: 'red'}}>La contraseña debe contener 5 digitos o más</h3>;
+  const errorFechaNacimientoMessage = <h3 style={{color: 'red'}}>Por favor coloque su fecha de nacimiento</h3>;
+  const errorTelefonoMessage = <h3 style={{color: 'red'}}>Por favor verifique los datos nuevamente. Debe contener mas de 5 digitos</h3>;
+  const errorDocumentoMessage = <h3 style={{color: 'red'}}>Debe contener mas de 6 digitos</h3>;
 
-  };
+
+};
+
 
   return (
     <div className="background-image">
@@ -79,7 +137,7 @@ const RegisterForm = () => {
             value={form.nombre}
             onChange={(e) => handleInputs("nombre", e.target.value)}
           />
-          {errors.nombre && <p className="error-message">{errors.nombre}</p>}
+          {errorNombre && <p className="error-message">{errorNombreMessage}</p>}
 
           <label className="label-field">Apellido:</label>
           <input
@@ -88,7 +146,7 @@ const RegisterForm = () => {
             value={form.apellido}
             onChange={(e) => handleInputs("apellido", e.target.value)}
           />
-          {errors.apellido && <p className="error-message">{errors.apellido}</p>}
+          {errorApellido && <p className="error-message">{errorApellidoMessage}</p>}
 
           <label className="label-field">Email:</label>
           <input
@@ -97,7 +155,7 @@ const RegisterForm = () => {
             value={form.email}
             onChange={(e) => handleInputs("email", e.target.value)}
           />
-          {errors.email && <p className="error-message">{errors.email}</p>}
+          {errorEmail && <p className="error-message">{errorEmailMessage}</p>}
 
           <label className="label-field">Contraseña:</label>
           <input
@@ -107,7 +165,7 @@ const RegisterForm = () => {
             value={form.password}
             onChange={(e) => handleInputs("password", e.target.value)}
           />
-          {errors.password && <p className="error-message">{errors.password}</p>}
+          {errorPassword && <p className="error-message">{errorPasswordMessage}</p>}
 
           <label className="label-field">Fecha de Nacimiento:</label>
           <input
@@ -117,7 +175,7 @@ const RegisterForm = () => {
             onChange={(e) => handleInputs("fechaNacimiento", e.target.value)}
             max={fechaActual}
           />
-          {errors.fechaNac && <p className="error-message">{errors.fechaNac}</p>}
+          {errorFechaNacimiento && <p className="error-message">{errorFechaNacimientoMessage}</p>}
 
           <label className="label-field">Teléfono:</label>
           <input
@@ -126,7 +184,7 @@ const RegisterForm = () => {
             value={form.telefono}
             onChange={(e) => handleInputs("telefono", e.target.value)}
           />
-          {errors.telefono && <p className="error-message">{errors.telefono}</p>}
+          {errorTelefono && <p className="error-message">{errorTelefonoMessage}</p>}
 
           <label className="label-field">Documento:</label>
           <input
@@ -135,7 +193,7 @@ const RegisterForm = () => {
             value={form.documento}
             onChange={(e) => handleInputs("documento", e.target.value)}
           />
-          {errors.documento && <p className="error-message">{errors.documento}</p>}
+          {errorDocumento&& <p className="error-message">{errorDocumentoMessage}</p>}
 
           <button className="submit-button" onClick={handleRegister}>
             Registrarse
@@ -146,7 +204,6 @@ const RegisterForm = () => {
             <br />
             <span>No recibiste el correo? <a onClick={() => console.log("asd")} style={{color: "white"}}>Volver a enviar</a></span>
             </div>}
-          {errors && <h3 style={{color: 'red'}}>Por favor verifique los datos nuevamente</h3>}
         </div>
       </div>
     </div>
